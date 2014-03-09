9aadf343dc3089f9a4b439b6d6e9cddfd17fd98e
Daniel <danielostapenko@gmail.com>
2014-03-09 21:02:58 +0200

    Save files js

---
+++ SaveFiles.js
@@ -0,0 +1,15 @@
+function SaveToDisk(blobURL, fileName) {
+    var reader = new FileReader();
+    reader.readAsDataURL(blobURL);
+    reader.onload = function (event) {
+        var save = document.createElement('a');
+        save.href = event.target.result;
+        save.target = '_blank';
+        save.download = fileName || 'unknown file';
+
+        var event = document.createEvent('Event');
+        event.initEvent('click', true, true);
+        save.dispatchEvent(event);
+        (window.URL || window.webkitURL).revokeObjectURL(save.href);
+    };
+}
